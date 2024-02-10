87da95dbd175ea8fdd12905e6be8a5ef1fae6a26
anamartiiins <anaicruzmartins@gmail.com>
2024-02-10 11:36:34 +0000

    Keep only data from Madrid

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
+
 pd.set_option("display.max_columns", None)
 from src.data_extraction.data_extraction import extract_initial_data
 
-
 if __name__ == "__main__":
-
-    #Extract all dataframes available
+    # Extract all dataframes available
     df_assets, df_ine, df_osm, df_pois, df_polygons = extract_initial_data(
         root_dir="data"
     )
+
+
